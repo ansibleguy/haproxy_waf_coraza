98f536fcc3be8cf637eecb3b644710d0a57f23cd
AnsibleGuy <guy@ansibleguy.net>
2024-12-26 23:37:53 +0100

    init

---
+++ filter_plugins/util.py
@@ -0,0 +1,13 @@
+from re import sub as regex_replace
+
+
+class FilterModule(object):
+
+    def filters(self):
+        return {
+            'safe_key': self.safe_key,
+        }
+
+    @staticmethod
+    def safe_key(key: str) -> str:
+        return regex_replace('[^0-9a-zA-Z]+', '', key.replace(' ', '_'))
