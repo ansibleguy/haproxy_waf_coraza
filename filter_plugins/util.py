c8a6b59937d4ff992e67ece70fd919205f3d9867
AnsibleGuy <guy@ansibleguy.net>
2024-12-27 21:13:41 +0100

    ignore duplicate apps

--- filter_plugins/util.py
+++ filter_plugins/util.py
@@ -6,8 +6,23 @@ class FilterModule(object):
     def filters(self):
         return {
             'safe_key': self.safe_key,
+            'unique_apps': self.unique_apps,
         }
 
     @staticmethod
     def safe_key(key: str) -> str:
         return regex_replace('[^0-9a-zA-Z_]+', '', key.replace(' ', '_'))
+
+    @staticmethod
+    def unique_apps(all_apps: list) -> list:
+        apps = []
+
+        for app in all_apps:
+            try:
+                if app['name'] not in apps:
+                    apps.append(app)
+
+            except KeyError:
+                pass
+
+        return apps
